65de0cde78d506cdcbdaef793ba71da63390847e
mariaa <mariaa@CT-MAN-00003>
2019-08-16 16:47:41 +0800

    france test 2

--- FranceFriesTest/FranceFriesTest/Program.cs
+++ FranceFriesTest/FranceFriesTest/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration; 
 using Microsoft.Extensions.Logging;
 
 namespace FranceFriesTest
